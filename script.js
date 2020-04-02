f54a1539c1d359f131b9b5e6feb45e23b43b5522
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 20:52:18 +0200

    Shader Program initial source

--- script.js
+++ script.js
@@ -23,6 +23,45 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT); 
 
 
+    /// The shaders  /////////////////////////////////////////////////////////////////////////////
+    /* 
+    A shader is a program, written using the OpenGL ES Shading Language (GLSL),
+    that takes information about the vertices that make up a shape and generates 
+    the data needed to render the pixels onto the screen: namely, the positions 
+    of the pixels and their colors.
+    */
+    
+
+    // Vertex shader program ///////////////
+    /*
+    Each time a shape is rendered, the vertex shader is run for each vertex in the shape.
+    Its job is to transform the input vertex from its original coordinate system into the
+    clipspace coordinate system used by WebGL
+    */
+    const vsSource = `
+    attribute vec4 aVertexPosition;    //receives vertex position values on this attribute
+
+    uniform mat4 uModelViewMatrix;      
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+    `;
+
+    // Fragment shader ///////////////
+    /*
+    The fragment shader is called once for every pixel on each shape to be drawn,
+    after the shape's vertices have been processed by the vertex shader.
+    Its job is to determine the color of that pixel
+    */
+    const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    `;
+
+
 
 }
 
