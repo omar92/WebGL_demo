bc8e72bb8b719649448b7fa82f10100f60a6164b
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 22:27:41 +0200

    add: Color attribute to programInfo object

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ function main() {
         program: shaderProgram,
         attribLocations: {
             vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+            vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
         },
         uniformLocations: {
             projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
