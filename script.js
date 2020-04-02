dea4d3e9051aa59f98a512781dca258fd645d679
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 22:04:51 +0200

    Load 3d Model

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ function drawScene(gl, programInfo, buffers) {
         zFar);
     
     
-    //>>Transform<<
+    //modelView Matrix >>Transform<<
     // Set the drawing position to the "identity" point, which is the center of the scene.
     const modelViewMatrix = mat4.create();
     
@@ -195,5 +195,30 @@ function drawScene(gl, programInfo, buffers) {
     mat4.translate(modelViewMatrix,     // destination matrix
         modelViewMatrix,     // matrix to translate
         [-0.0, 0.0, -6.0]);  // amount to translate  >> transform.Position<<
+    
+    
+
+    //Load the 3d model
+    // Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute.
+    
+    const numComponents = 2;  // pull out 2 values per iteration
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to get from one set of values to the next
+    // 0 = use type and numComponents above
+    const offset = 0;         // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+    
+    
+    
     
 }
