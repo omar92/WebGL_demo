747ca6154eee803ec3552ce0ee712684473d7ff0
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 20:35:50 +0200

    -get webgl context refrence - set clear color  == blue - clear color buffer

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function main() {
                                                          // input[name='login']  will search for input with name login
     
     if (!canvas) { console.error("Couldnt find Canvas"); return }  //terminate if canvas not found
-    console.log(canvas);
+
+    
+    // Initialize the GL context
+    const gl = canvas.getContext("webgl"); // before we were using '2d'
+
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 2.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT); 
+
+
+
 }
 
 window.onload = main;
