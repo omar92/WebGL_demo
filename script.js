79f16b0af9248ad7a5f71ce25104e0e2d56ca8b6
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 21:10:40 +0200

    use initShaderProgram to create shaderProgramm get refrence to the shader attributes and Uniforms backage thim in object to easily use them

--- script.js
+++ script.js
@@ -61,6 +61,31 @@ function main() {
     }
     `;
 
+
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+    //after creating the shaderProgramm we need to get refrence to the parametters inside the shader to send the data to it
+
+    var _vertexPosition = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
+    var _projectionMatrix= gl.getUniformLocation(shaderProgram, 'uProjectionMatrix');
+    var _modelViewMatrix = gl.getUniformLocation(shaderProgram, 'uModelViewMatrix');
+
+
+    //create simple opject to organize the data 
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: _vertexPosition,
+        },
+        uniformLocations: {
+            projectionMatrix: _projectionMatrix,
+            modelViewMatrix: _modelViewMatrix,
+        },
+    };
+
+
+    
+
 } window.onload = main; // call main onLoad
 
 
