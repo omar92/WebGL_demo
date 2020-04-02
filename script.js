1dd515c51af74a1346cc37518ecc67c1d603981a
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 20:57:25 +0200

    Add : function to Initialize a shader program

--- script.js
+++ script.js
@@ -61,11 +61,34 @@ function main() {
     }
     `;
 
+} window.onload = main; // call main onLoad
+
+//
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+    //load  vertex and fragment shader 
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    // Create the shader program
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
 
 
+    // If creating the shader program failed, alert
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
 }
 
-window.onload = main;
+
+
 
 
 
