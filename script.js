1fa34e31eaa675519849b155eaa6bd1dce3b40d2
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 22:16:32 +0200

    create buffer to hold vertices colors

--- script.js
+++ script.js
@@ -150,9 +150,23 @@ function initBuffers(gl) {
     gl.bufferData(gl.ARRAY_BUFFER,
         new Float32Array(positions),
         gl.STATIC_DRAW);
+    
+    //each color maps for one vertix from above
+    const colors = [
+        1.0, 1.0, 1.0, 1.0,    // white
+        1.0, 0.0, 0.0, 1.0,    // red
+        0.0, 1.0, 0.0, 1.0,    // green
+        0.0, 0.0, 1.0, 1.0,    // blue
+    ];
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    
 
     return {
         position: positionBuffer,
+        color: colorBuffer,
         vertexCount: positions.length/2
     };
 }
