a725775687417adcede7942c37a2559977784252
omar abdelzaher <o.abdelzaher@entrenchtech.com>
2020-04-02 22:08:37 +0200

    use shader program to draw using loaded data

--- script.js
+++ script.js
@@ -153,6 +153,7 @@ function initBuffers(gl) {
 
     return {
         position: positionBuffer,
+        vertexCount: positions.length/2
     };
 }
 
@@ -204,8 +205,7 @@ function drawScene(gl, programInfo, buffers) {
     const numComponents = 2;  // pull out 2 values per iteration
     const type = gl.FLOAT;    // the data in the buffer is 32bit floats
     const normalize = false;  // don't normalize
-    const stride = 0;         // how many bytes to get from one set of values to the next
-    // 0 = use type and numComponents above
+    const stride = 0;         // how many bytes to jump over
     const offset = 0;         // how many bytes inside the buffer to start from
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
@@ -219,6 +219,21 @@ function drawScene(gl, programInfo, buffers) {
         programInfo.attribLocations.vertexPosition);
     
     
+    // Tell WebGL to use our program when drawing
+    gl.useProgram(programInfo.program);
+    // Set the shader uniforms
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix);
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix);
     
-    
+    {
+        const offset = 0;
+        const vertexCount = buffers.vertexCount;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
 }
